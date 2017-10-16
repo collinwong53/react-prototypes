1224bed16603fb54cd915dfb5d85ad8e017c814e
collinwong53 <collinwong53@gmail.com>
2017-10-16 00:25:35 -0700

    started on prototype

--- src/index.js
+++ src/index.js
@@ -5,3 +5,18 @@ ReactDOM.render(
     <h1 className="container">React Prototypes!</h1>,
     document.getElementById('root')
 );
+function greeting(object){
+    const element = (
+        <div>
+            <h1>Hello, {object.name} </h1>
+            <h2 className={text-muted}>Your lucky number is object.luckyNumber</h2>
+        </div>
+    )
+}
+const name = 'Collin';
+const luckyNumber =()=>{
+    return Math.random()*500 >>1;
+}
+
+const userObject = {name,luckyNumber};
+
