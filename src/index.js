f632f74871460e891bff5d71dbfd12c8815a10d9
collinwong53 <collinwong53@gmail.com>
2017-10-16 08:44:43 -0700

    got everything except delete method

--- src/index.js
+++ src/index.js
@@ -9,14 +9,18 @@ function greeting(object){
     const element = (
         <div>
             <h1>Hello, {object.name} </h1>
-            <h2 className={text-muted}>Your lucky number is object.luckyNumber</h2>
+            <h2 className="text-muted">Your lucky number is {object.luckyNumber()}</h2>
         </div>
+    );
+    ReactDOM.render(
+        element,document.getElementById('root')
     )
 }
 const name = 'Collin';
 const luckyNumber =()=>{
-    return Math.random()*500 >>1;
-}
+    return Math.random()*1000 >>1;
+};
 
 const userObject = {name,luckyNumber};
 
+greeting(userObject);
