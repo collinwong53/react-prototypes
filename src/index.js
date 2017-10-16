26a121c051c294d93de8ecd946a80ac9fe0e2420
collin_wong <collinwong53@gmail.com>
2017-10-16 11:21:20 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
-    document.getElementById('root')
-);
 function greeting(object){
     const element = (
         <div>
