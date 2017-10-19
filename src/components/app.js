f089efe9fa1f0e41ba6805c4683e01ba4111361a
collin_wong <collinwong53@gmail.com>
2017-10-19 14:47:02 -0700

    functional-components-Collin Wong

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,26 @@
-
+import React from 'react';
+import Table from './table';
+const students =
+    [{
+        name : 'Billy B',
+        course: 'Criminal Justice',
+        grade: 94
+    },
+    {
+        name:'Pikachu',
+        course:'Forensic Science',
+        grade: 98
+    },
+    {
+        name: 'Superman',
+        course: 'Creative Writing',
+        grade: 86
+    }]
+export default() => {
+    return (
+        <div className="container">
+        <h1>Student Grade Table</h1>
+        <Table data={students}/>
+        </div>
+    )}
 
